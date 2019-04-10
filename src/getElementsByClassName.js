0acb273403dbf51073f21c0c75738fe2e2507423
Konstantin and Mavroian <konst2011+mavflorin@hotmail.de>
2019-04-10 11:47:24 +0900

    GetElementsByClassName Complete

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,17 @@
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
+const getElementsByClassName = (className) => {
+  const nodeList = [];
+  const recursive = (node) => {
+    for (let i = 0; i < node.childNodes.length; i++) {
+      const nodes = node.childNodes[i];
+      if (nodes.classList && nodes.classList.contains(className)) {
+        nodeList.push(nodes);
+      }
+      recursive(nodes);
+    }
+  };
+  recursive(document);
+
+  return nodeList;
 };
 
 module.exports = { getElementsByClassName };
